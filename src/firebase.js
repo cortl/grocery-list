28e2c5649a4d0ccc770783dd74decc33efcdd905
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2021-09-29 08:47:30 -0500

    use correct .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY_DEV,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN_DEV,
-    databaseURL: process.env.REACT_APP_DATABASE_URL_DEV,
-    projectId: process.env.REACT_APP_PROJECT_ID_DEV,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET_DEV,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_DEV,
-    appId: process.env.REACT_APP_APP_ID_DEV
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 
